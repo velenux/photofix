672bd305f811b1f002364e5aa53e1bd14dcb30cb
Gilberto Ficara <g.ficara@stardata.it>
2015-08-31 12:21:32 +0200

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-python testfix.py test 2>errors.log | tee run.log
+if [ ! -d "$1" ]; then
+  echo "Please run $0 <directory>"
+  echo "You'll find errors in errors.log and the output will be logged in run.log"
+  exit 1
+fi
+
+python photofix.sh "$1" 2>errors.log | tee run.log
